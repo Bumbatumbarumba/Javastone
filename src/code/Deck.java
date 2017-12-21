5198730a8f0d494dba12b2d80914bf3137005150
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-12-20 21:44:21 -0500

    joachim is a fuck

--- src/code/Deck.java
+++ src/code/Deck.java
@@ -6,16 +6,34 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Deck {
-	ArrayList<Cards> deck = new ArrayList<Cards>();
+	static List<Cards> deck = new ArrayList<Cards>();
 	
-	public void addCard(Cards card){
+	public static void addCard(Cards card){
 		deck.add(card);
-		sortDeck(deck);
-	}
+		sortDeck();
+	}//end of addCard
 	
-	public void sortDeck(ArrayList<Cards> deck){
+	public static void sortDeck(){
 		//sorts the deck first by mana cost, then alphabetically
-	}
+	}//end of sortDeck
+	
+	public static void shuffleDeck(){
+		int deckSize = deck.size();
+		Random rand = new Random();
+		rand.nextInt();
+		for (int i = 0; i < deckSize; i++){
+			int change = i + rand.nextInt(deckSize-1);
+			swap(i, change);
+		}
+	}//end of shuffleDeck
+	
+	private static void swap(int i, int change){
+		Cards helper = deck.get(i);
+		deck.set(i, deck.get(change));
+		deck.set(change, helper);
+	}//end of swap
 }
