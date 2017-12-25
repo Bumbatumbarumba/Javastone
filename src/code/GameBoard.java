1fc8e6996e2aa2f514e6e73c0c00607067c044f3
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-12-25 18:13:39 -0500

    added some stuff such as drawing cards

--- src/code/GameBoard.java
+++ src/code/GameBoard.java
@@ -7,6 +7,9 @@
  */
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameBoard {
 	// general stuff
 	public static int playerTurn = 1;
@@ -18,5 +21,30 @@ public class GameBoard {
 
 	// player one stuff
 	public static int p1mana = 0;
-
-}
+	public static List<Cards> player1hand = new ArrayList<Cards>();
+	public static List<Cards> player1board = new ArrayList<Cards>();
+	
+	// player two stuff
+	public static int p2mana = 0;
+	public static List<Cards> player2hand = new ArrayList<Cards>();
+	public static List<Cards> player2board = new ArrayList<Cards>();
+	
+	public static void addCardToHand1(Cards card){
+		if (player1hand.size() < 10){
+			player1hand.add(card);
+		}
+		else{
+			//card gets drawn but not added
+			//I don't think I need to include this, since when a card
+			//is drawn, it just gets removed automatically; it is
+			//prolly worth keeping for when I want to add a burn animation
+		}
+	}
+	
+	public static void playMinion1(Cards minion){
+		if (player1board.size() < 7){
+			player1board.add(minion);
+		}
+	}
+	
+}//end of gameBoard class
