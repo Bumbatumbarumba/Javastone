27305e91b58603ee3d9fa3596adc6947388dac2f
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-12-25 18:13:16 -0500

    added some new minions

--- src/code/AllMinions.java
+++ src/code/AllMinions.java
@@ -10,6 +10,94 @@ public class AllMinions {
 	//do something maybe
 }//end of AllMinions
 
+//=================================== Every minion ===================================
+
+//A
+
+//B
+
+//C
+class ChillwindYeti extends Cards{
+	ChillwindYeti(){
+		this.cardName = "Chillwind Yeti";
+		this.manaCost = 4;
+		this.attack = 4;
+		this.health = 5;
+		this.heroClass = "Neutral";
+		this.keyword = "None";
+		this.rarity = "Common";
+		this.summoningSickness = true;
+	}//end of ChillwindYeti constructor
+}//end of ChillwindYeti class
+
+//D
+
+//E
+
+//F
+
+//G
+
+//H
+
+//I
+
+//J
+
+//K
+
+//L
+
+//M
+
+//N
+class NoviceEngineer extends Cards{
+	NoviceEngineer(){
+		this.cardName = "Novice Engineer";
+		this.manaCost = 2;
+		this.attack = 1;
+		this.health = 1;
+		this.heroClass = "Neutral";
+		this.keyword = "Battlecry";
+		this.rarity = "Common";
+		this.summoningSickness = true;
+	}//end of StonetuskBoar constructor
+	
+	//draws a card to the player's hand
+	public void battlecry(){
+		GameBoard.addCardToHand1(Deck.drawCard());
+	}
+}//end of StonetuskBoar class
+
+//O
+
+//P
+
+//Q
+
+//R
+
+//S
+class StonetuskBoar extends Cards{
+	StonetuskBoar(){
+		this.cardName = "Stonetusk Boar";
+		this.manaCost = 1;
+		this.attack = 1;
+		this.health = 1;
+		this.heroClass = "Neutral";
+		this.keyword = "None";
+		this.rarity = "Common";
+		this.summoningSickness = false;
+	}//end of StonetuskBoar constructor
+}//end of StonetuskBoar class
+
+//T
+
+//U
+
+//V
+
+//W
 class Wisp extends Cards{
 	Wisp(){
 		this.cardName = "Wisp";
@@ -21,4 +109,12 @@ class Wisp extends Cards{
 		this.rarity = "Common";
 		this.summoningSickness = true;
 	}//end of Wisp constructor
-}
+}//end of Wisp class
+
+//X
+
+//Y
+
+//Z
+
+
