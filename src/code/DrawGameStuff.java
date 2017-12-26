ab1369ed766b2f1fb0a9b702ced4a281280ed194
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-12-26 15:56:02 -0500

    added basic jframe stuff

--- src/code/DrawGameStuff.java
+++ src/code/DrawGameStuff.java
@@ -3,9 +3,24 @@
  * Holds all the stuff related to drawing the board, the deck
  * creation screen, and whatever other stuff I think of eventually
  */
-
 package code;
 
-public class DrawGameStuff {
-
+import javax.swing.JFrame;
+//draws whatever stuff needs to be drawn
+public class DrawGameStuff extends JFrame{
+	public static void main (String [] args){
+		BoardGUI.createBoard();
+	}//end of main method
 }//end of DrawGameStuff
+
+//draws the game board
+class BoardGUI extends JFrame{
+	public static BoardGUI bgui = new BoardGUI();
+	
+	public static void createBoard(){
+		bgui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bgui.setSize(500, 500);
+		bgui.setVisible(true);
+		bgui.setTitle("Board");
+	}//end of createBoard
+}//end of BoardGUI class
