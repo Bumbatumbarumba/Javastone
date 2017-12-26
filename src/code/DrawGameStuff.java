bba08025f42c3a8f01dc5d36ec2b65a6fba13169
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-12-25 21:42:27 -0500

    created this class to hold visual stuff

---
+++ src/code/DrawGameStuff.java
@@ -0,0 +1,11 @@
+/* Created by Bartosz Kosakowski
+ * 25/12/2017
+ * Holds all the stuff related to drawing the board, the deck
+ * creation screen, and whatever other stuff I think of eventually
+ */
+
+package code;
+
+public class DrawGameStuff {
+
+}//end of DrawGameStuff
