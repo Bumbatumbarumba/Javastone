3e0e309a12d9da0c00b8e87fdbd956f2654e1b75
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-12-27 14:22:49 -0500

    did some stuff, then undid some stuff

--- src/code/DrawGameStuff.java
+++ src/code/DrawGameStuff.java
@@ -5,7 +5,11 @@
  */
 package code;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 //draws whatever stuff needs to be drawn
 public class DrawGameStuff extends JFrame{
 	public static void main (String [] args){
@@ -15,12 +19,55 @@ public class DrawGameStuff extends JFrame{
 
 //draws the game board
 class BoardGUI extends JFrame{
-	public static BoardGUI bgui = new BoardGUI();
+	private static BoardGUI bgui = new BoardGUI();
+	private static Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	
 	public static void createBoard(){
 		bgui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		bgui.setSize(500, 500);
+		bgui.setSize(dim.width, dim.height);
 		bgui.setVisible(true);
 		bgui.setTitle("Board");
 	}//end of createBoard
 }//end of BoardGUI class
+
+/*
+//used to let the user know something, eg, bad login credentials, etc
+class GenericMessage extends JFrame{
+	private GenericMessage msg;
+	private JLabel genericMsg;
+	private JButton closeMsg;
+	
+	public GenericMessage(String message){
+		msg = new GenericMessage(message);
+		
+		setLayout(new FlowLayout());
+		
+		genericMsg = new JLabel(message);
+		msg.add(genericMsg);
+		
+		closeMsg = new JButton("Okay");
+		msg.add(closeMsg);
+		
+		PressOkay e = new PressOkay();
+		closeMsg.addActionListener(e);
+	}//end of constructor
+	
+	//this class implements the ActionListener interface so that we can
+	//give the "okay" button the ability to close just the message window
+	public class PressOkay implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			msg.dispose();
+		}
+	}//end of PressOkay
+	
+	public void createMessage(String title){
+		msg.pack();
+		msg.setLocationRelativeTo(null);
+		msg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		msg.setSize(500, 500);
+		msg.setVisible(true);
+		msg.setTitle(title);
+	}//creates the message window
+}//end of GenericMessage class
+*/
